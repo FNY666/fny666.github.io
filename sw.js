5ac2247500152c8876cba62a69b83b0185f36c42
FNY666 <90173163+FNY666@users.noreply.github.com>
2026-08-28 08:11:36 +0800

    Update Pixel Brawl: sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 // 像素乱斗 Service Worker：安装后可离线游玩
 'use strict';
 
-const VERSION = 'pixel-brawl-v53';
+const VERSION = 'pixel-brawl-v56';
 const CACHE_NAME = VERSION;
 // 注意：必须与 index.html 中的资源版本号一致
 const ASSETS = [
   './',
   './index.html',
-  './style.css?v=15',
-  './game.js?v=53',
+  './style.css?v=16',
+  './game.js?v=56',
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png'
